d891ba1b56e4d836ec2e72e1aa8d3469c1ffc6bd
MLGLoL09 <153075796+MLGLoL09@users.noreply.github.com>
2024-05-23 10:21:26 +0200

    Methoden

---
+++ InstaStats.java
@@ -0,0 +1,14 @@
+public class InstaStats {
+    public static int buchstabeZaehlen(char letter, String text) {
+        int häufigkeitBuchstabe = 0;
+
+        return häufigkeitBuchstabe;
+    }
+
+    public static String statistik(String text) {
+        String häufigkeiten = "a: ";
+
+        return häufigkeiten;
+    }
+
+}
