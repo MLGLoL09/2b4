2b13509f75885d9a622d9fc8e9e57d8cf754a1d6
MLG_LoL08 <mlglol@outlook.at>
2024-05-24 19:19:42 +0200

    final

--- InstaStats.java
+++ InstaStats.java
@@ -1,16 +1,26 @@
 public class InstaStats {
     public static int buchstabeZaehlen(char letter, String text) {
-        int häufigkeitBuchstabe = 0;
+        int indexMerken = 0;
+        int indexZaehler = 0;
+        int length = text.length();
+        while (indexZaehler < length) {
+            if (text.charAt(indexZaehler) == letter) {
+                indexMerken++;
+            }
+            indexZaehler++;
+        }
 
-        return häufigkeitBuchstabe;
-    }
-
-    public static String statistik(String text) {
-        String häufigkeiten = "a: ";
-        char letter;
         
-
-        return häufigkeiten;
+        return indexMerken;
     }
-    tests
+
+    public static String statistik2(String text) {
+        String abc = "abcdefghijklmnopqrstuvwxyzäüö";
+        String haufigkeiten = "STATISTIK";
+        for (int i =0; i < abc.length(); i++) {
+            char buchstabe = abc.charAt(i);
+            haufigkeiten += "\n" + buchstabe + ": " + buchstabeZaehlen(buchstabe, text);
+        }
+        return haufigkeiten;
+    }
 }
--- InstaTest.java
+++ InstaTest.java
@@ -3,7 +3,7 @@ import javax.swing.JOptionPane;
 public class InstaTest {
     public static void main(String[] args) {
         String text = JOptionPane.showInputDialog(null, "Geben Sie hier Ihren Text ein.");
-        
-
+        String x = InstaStats.statistik2(text);
+        JOptionPane.showMessageDialog(null, x);
     }
 }
