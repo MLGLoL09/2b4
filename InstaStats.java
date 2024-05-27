d3bfabbd3050680840e4b81ad60037f3ff50f195
MLGLoL09 <153075796+MLGLoL09@users.noreply.github.com>
2024-05-27 11:02:04 +0200

    comments

--- InstaStats.java
+++ InstaStats.java
@@ -1,4 +1,15 @@
+/**
+ * ueberprueft wie oft es jeden Buchstaben im Text gibt.
+ * @author Sebastian Weigl
+ * @version 05-19-2024
+ */
 public class InstaStats {
+    /**
+     * zaehlt wie viele buchsaben es von einem bestimmten giebt im text
+     * @param letter uebernimmt den buchstaben der ueberprueft werden soll
+     * @param text uebernimmt den text in welchem der Buchstabe ueberprueft werden soll
+     * @return gibt zurueck wie oft es den Buchstaben gibt
+     */
     public static int buchstabeZaehlen(char letter, String text) {
         int indexMerken = 0;
         int indexZaehler = 0;
@@ -9,11 +20,13 @@ public class InstaStats {
             }
             indexZaehler++;
         }
-
-        
         return indexMerken;
     }
-
+    /**
+     * macht die Statistik
+     * @param text uebernimmt den Text von welchem die Buchstaben ueberprueft werden sollen
+     * @return gibt die Tabelle mit den Werten wie oft es welchen buchstaben gibt zurueck
+     */
     public static String statistik2(String text) {
         String abc = "abcdefghijklmnopqrstuvwxyzäüö";
         String haufigkeiten = "STATISTIK";
--- InstaTest.java
+++ InstaTest.java
@@ -1,6 +1,13 @@
 import javax.swing.JOptionPane;
-
+/**
+ * ueberprueft wie oft es jeden Buchstaben im Text gibt.
+ * @author Sebastian Weigl
+ * @version 05-19-2024
+ */
 public class InstaTest {
+    /**
+     * uebernimmt den Text den du ueberpruefen willst und gibt die Statistik zurueck
+     */
     public static void main(String[] args) {
         String text = JOptionPane.showInputDialog(null, "Geben Sie hier Ihren Text ein.");
         String x = InstaStats.statistik2(text);
